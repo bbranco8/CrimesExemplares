fa4f405f4a87a9b451105c581cb77c95ef886ff3
MariaMiguel0510 <mariammpedro04@gmail.com>
2024-12-08 12:35:55 +0000

    Alterações quizz
    Mudei o tamanho do contentor e algumas propriedades de css

--- JS/homepage.js
+++ JS/homepage.js
@@ -14,6 +14,9 @@ function esconderTodos() {
 }
 esconderTodos();
 
+
+
+
 // Função para calcular a distância entre dois pontos
 function dist(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
